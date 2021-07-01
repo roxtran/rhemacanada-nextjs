4c120258d979834d5b4ecb2a930d1d7ed6ea222a
Roxanne <tran.rox@gmail.com>
2021-07-01 04:17:01 -0400

    updated sticky navbar

---
+++ components/MetaHead.js
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+
+const MetaHead = () => {
+  return (
+    <Head>
+      <title>Rhema Canada - What We Do, About Us, Our LeaderShip...</title>
+      <meta
+        name='description'
+        content='Rhema Canada is a world class people-centered organization with people at the heart of what we do. We change lives in the community and the world.'
+      />
+      <meta property='og:type' content='website' />
+      <meta
+        name='og:title'
+        property='og:title'
+        content='Rhema Canada - What We Do, About Us, Our LeaderShip...'
+      />
+      <meta
+        name='og:description'
+        property='og:description'
+        content='Rhema Canada is a world class people-centered organization with people at the heart of what we do. We change lives in the community and the world.'
+      />
+      <meta property='og:site_name' content='RCM' />
+      <meta property='og:url' content='https://rhemacanada.ca/' />
+      <meta name='twitter:card' content='summary' />
+      <meta
+        name='twitter:title'
+        content='Rhema Canada - What We Do, About Us, Our LeaderShip...'
+      />
+      <meta
+        name='twitter:description'
+        content='Welcome to Rhema Christian Ministries, a vibrant Spirit-filled church community designed to connect people to Jesus and to each other through authentic relationships.'
+      />
+      <meta name='twitter:site' content='RCM' />
+      <meta name='twitter:creator' content='Roxanne' />
+      <link rel='icon' type='image/png' href='/favicon.ico' />
+      <link rel='apple-touch-icon' href='/favicon.ico' />
+      <link rel='stylesheet' href='' />
+      <meta property='og:image' content='/img/og/home.jpg' />
+      <meta name='twitter:image' content='' />
+      <link rel='canonical' href='https://rhemacanada.ca/' />
+      <script type='text/javascript' src=''></script>
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+        rel='stylesheet'
+      />
+    </Head>
+  )
+}
+
+export default MetaHead
