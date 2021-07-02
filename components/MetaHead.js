42465312c4bed5ec447d41fec7c13c3e83410d59
Roxanne <tran.rox@gmail.com>
2021-07-02 14:46:15 -0400

    added Google Analytics

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -47,12 +47,14 @@ const MetaHead = () => {
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
       <script
         async
-        src='https://www.googletagmanager.com/gtag/js?id=G-MGWD29X74W'
+        src='https://www.googletagmanager.com/gtag/js?id=UA-27709648-8'
       ></script>
       <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-MGWD29X74W');
+        window.dataLayer = window.dataLayer || []
+        function gtag() {dataLayer.push(arguments)}
+        gtag('js', new Date())
+        
+        gtag('config', 'UA-27709648-8')
       </script>
     </Head>
   )
